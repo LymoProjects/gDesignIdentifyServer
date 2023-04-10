06bf05b9e594e0bacdfb74c93f8e8fd714812bd2
lymo <87603845+LymoProjects@users.noreply.github.com>
2023-04-10 23:38:51 +0800

    feat: add http:get server for getting bin files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
 #include <co/http.h>
-#include <co/time.h>
 
 #include "httpServerConf.hpp"
 
 auto main() -> int {
-    http::Server().on_req(gd__::httpServerConf::httpOnReq).start("0.0.0.0", 9193);
+    http::Server().on_req(gd__::httpServerConf::httpOnReq).start("0.0.0.0", 9194);
 
-    while (true) {
-        sleep::sec(100);
-    }
+    so::easy(".", "0.0.0.0", 9195);
 
     return 0;
 }
